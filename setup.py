e81b5754fa2d6960bb77da9614666955f79b34e5
Geert-Jan Huizing <huizing@ens.fr>
2021-08-05 16:34:49 +0200

    made pip installable
    Former-commit-id: 8d2b190fc35bda4039cabbbf6d71c595cb46c98c

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='scmiot',
+      version='0.0.1',
+      description='Single-cell multi-omics integration with optimal transport',
+      author='Geert-Jan Huizing',
+      author_email='huizing@ens.fr',
+      packages=['scmiot'],
+    )
