fdd7f840c0935c49ccdbfac4a1b7f55adb9379b3
Geert-Jan Huizing <huizing@ens.fr>
2021-08-23 15:32:50 +0200

    silhouette-based clustering heuristic
    Former-commit-id: 3829a8ee13f5deda2f205aa6d44b674293e417b3

--- scmiot/tl.py
+++ scmiot/tl.py
@@ -2,8 +2,9 @@
 import scanpy as sc
 import muon as mu
 import anndata as ad
-from sklearn.metrics import r2_score, explained_variance_score
+from sklearn.metrics import r2_score, explained_variance_score, silhouette_score
 import matplotlib.pyplot as plt
+import numpy as np
 
 def variance_explained(mdata, score_function='explained_variance_score'):
     print('experimental, i have to test this function')
@@ -35,3 +36,43 @@ def variance_explained(mdata, score_function='explained_variance_score'):
     plt.colorbar()
     plt.show()
     return score
+
+def leiden(mdata, n_components=None, obsm='W_OT', resolution=1):
+    joint_embedding = ad.AnnData(mdata.obsm[obsm].cpu().numpy(), obs=mdata.obs)
+    if n_components==None:
+        n_components = joint_embedding.n_vars
+    sc.pp.neighbors(joint_embedding, use_rep="X", n_neighbors=n_components)
+    sc.tl.leiden(joint_embedding, resolution=resolution)
+    mdata.obs['leiden'] = joint_embedding.obs['leiden']
+
+def sil_score(mdata, obsm='W_OT', obs='leiden'):
+    return silhouette_score(mdata.obsm[obsm], mdata.obs[obs])
+
+def best_leiden_resolution(mdata, obsm='W_OT', resolution_range=None, return_second_max=True, n_components=None):
+    if resolution_range==None:
+        resolution_range = 10.**np.linspace(-2, 1, 20)
+    sils = []
+
+    joint_embedding = ad.AnnData(mdata.obsm[obsm].cpu().numpy(), obs=mdata.obs)
+    if n_components==None:
+        n_components = joint_embedding.n_vars
+    sc.pp.neighbors(joint_embedding, use_rep="X", n_neighbors=n_components)
+
+    for res in resolution_range:
+        sc.tl.leiden(joint_embedding, resolution=res)
+        sils.append(silhouette_score(joint_embedding.X, joint_embedding.obs['leiden']))
+    plt.xscale('log')
+    plt.scatter(resolution_range, sils)
+    plt.plot(resolution_range, sils)
+    plt.ylabel('Silhouette score')
+    plt.xlabel('Resolution')
+    plt.show()
+
+    maxes = []
+    for i in range(1, len(sils)-1):
+        if sils[i] > sils[i+1] and sils[i] >= sils[i-1]:
+            maxes.append(i)
+    if return_second_max:
+        return resolution_range[maxes[1]]
+    else:
+        return resolution_range[maxes]
