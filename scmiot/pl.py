4c2d4dd42d8e0d539cedd455ac8dc498fcf3313b
Geert-Jan Huizing <huizing@ens.fr>
2021-10-26 17:08:39 +0200

    doc pl
    Former-commit-id: 648e9c192201f5930e5067776c6953286f2d44eb
    Former-commit-id: 87e3fe8925547937cf9ca58a05847b7f40310fff

--- scmiot/pl.py
+++ scmiot/pl.py
@@ -5,10 +5,20 @@ import anndata as ad
 import numpy as np
 
 
-def heatmap(mdata, obsm, groupby):
-    try:
-        joint_embedding = ad.AnnData(mdata.obsm[obsm].cpu().numpy(), obs=mdata.obs)
-    except:
-        joint_embedding = ad.AnnData(mdata.obsm[obsm], obs=mdata.obs)
-    idx = joint_embedding.var_names[joint_embedding.X.std(0).argsort()[::-1]]
-    sc.pl.heatmap(joint_embedding, idx, groupby=groupby, cmap='viridis')
+def heatmap(mdata: mu.MuData, obsm: str, groupby: str, cmap: str = 'viridis', sort_var: bool = False) -> None:
+    """Produce a heatmap of an embedding
+
+    Args:
+        mdata (mu.MuData): Input data
+        obsm (str): The embedding
+        groupby (str): What to group by
+        cmap (str, optional): Color map. Defaults to 'viridis'.
+        sort_var (bool, optional): Sort dimensions by variance. Defaults to False.
+    """
+    
+    joint_embedding = ad.AnnData(mdata.obsm[obsm], obs=mdata.obs)
+    if sort_var:
+        idx = joint_embedding.var_names[joint_embedding.X.std(0).argsort()[::-1]]
+    else:
+        idx = joint_embedding.var_names
+    sc.pl.heatmap(joint_embedding, idx, groupby=groupby, cmap=cmap)
