f148128a23b30721b9a7393864b5cf9f9f68e58e
Geert-Jan Huizing <huizing@ens.fr>
2022-04-28 12:23:15 +0200

    made scores more coherent
    Former-commit-id: f88b84247045657d3ddcb5f3ab590caa869341c1
    Former-commit-id: 643c977a8f718074750b806832d0a0646a066a42

--- mowgli/score.py
+++ mowgli/score.py
@@ -1,10 +1,11 @@
 import numpy as np
+from scipy.stats import pearsonr, spearmanr
 from sklearn.metrics import silhouette_score
 
 ############################ BASED ON AN EMBEDDING ############################
 
 
-def silhouette(
+def embedding_silhouette_score(
     embedding: np.ndarray,
     labels: np.ndarray,
     metric: str = "euclidean",
@@ -32,7 +33,7 @@ def silhouette(
 ################################ BASED ON A KNN ###############################
 
 
-def purity(knn: np.ndarray, labels: np.ndarray) -> float:
+def knn_purity_score(knn: np.ndarray, labels: np.ndarray) -> float:
     """Compute the kNN purity score, averaged over all observations.
     For one observation, the purity score is the percentage of
     nearest neighbors that share its label.
@@ -63,6 +64,57 @@ def purity(knn: np.ndarray, labels: np.ndarray) -> float:
         scores.append(np.mean(matches))
 
     # Return the average purity.
+    return np.mean(scores)
+
+
+def knn_prediction_score(
+    X: np.ndarray,
+    knn: np.ndarray,
+    correlation_fn: str = "pearson",
+) -> float:
+    """Compute the prediction score given the input data and a kNN, averaged
+    over observations. For one observation, this is the correlation between
+    the observation and the average of its neighbors. In other words, this
+    measures the capacity of a kNN to impute missing values.
+
+    Args:
+        X (np.ndarray):
+            The input data. (n_obs, n_features)
+        knn (np.ndarray):
+            The knn (n_obs, k). The i-th row contains the indices
+            to the k nearest neighbors.
+        correlation_fn (str, optional):
+            The function to use for correlation, either 'pearson'
+            or 'spearman'. Defaults to "pearson".
+
+    Returns:
+        float: The averaged knn prediction score.
+    """
+    # Check that the correlation function is correct.
+    assert correlation_fn == "pearson" or correlation_fn == "spearman"
+
+    # Define the correlation function.
+    corr = pearsonr if correlation_fn == "pearson" else spearmanr
+
+    # Check that the input's dimensions are correct.
+    assert X.shape[0] == knn.shape[0]
+
+    # Initialize the prediction scores.
+    scores = []
+
+    # Iterate over the observations.
+    for i, neighbors in enumerate(knn):
+
+        # Predict cell i from its neighbors.
+        x_predicted = np.mean(X[neighbors], axis=0)
+
+        # Check that the prediction has the right shape.
+        assert x_predicted.shape[0] == X.shape[1]
+
+        # Add the correlation score.
+        scores.append(corr(x_predicted, X[i]))
+
+    # Return the average prediction score
     return np.mean(scores)
 
 
