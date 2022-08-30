466e39e6ddef8cafa7fc640851aa382b153256b8
Geert-Jan Huizing <huizing@ens.fr>
2022-08-30 13:54:00 +0200

    add requirements
    Former-commit-id: 5588688b911a0bb805de153f7698be7a03e8549b
    Former-commit-id: 7e6da7590e81f09924f046ce5100ed86e2538327

--- setup.py
+++ setup.py
@@ -2,10 +2,25 @@
 
 from setuptools import setup, find_packages
 
-setup(name='mowgli',
-      version='0.0.1',
-      description='Mowgli: Multi Omics Wasserstein inteGrative anaLysIs.',
-      author='Geert-Jan Huizing',
-      author_email='huizing@ens.fr',
-      packages=['mowgli'],
-    )
+setup(
+    name="mowgli",
+    version="0.0.1",
+    description="Mowgli: Multi Omics Wasserstein inteGrative anaLysIs.",
+    author="Geert-Jan Huizing",
+    author_email="huizing@ens.fr",
+    packages=["mowgli"],
+    install_requires=[
+        "torch",
+        "numpy",
+        "pandas",
+        "sklearn",
+        "muon",
+        "tqdm",
+        "scanpy",
+        "anndata",
+        "matplotlib",
+        "plotly",
+        "scipy",
+        "sknetwork",
+    ],
+)
