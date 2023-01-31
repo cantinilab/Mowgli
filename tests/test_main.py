e7d2678153209326489dc19dae77de00d0cd686a
Geert-Jan Huizing <huizing@ens.fr>
2023-01-31 17:02:08 +0100

    added some more tests to improve coverage
    Former-commit-id: 71ee6ff3de9c610852da3e6e9a13c64c337b09f9
    Former-commit-id: 1ed7fa63d4a48e3d54dec5e0f9f45da18a31b73f

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,26 +3,56 @@ import muon as mu
 import anndata as ad
 import numpy as np
 
+n_cells, n_genes, n_peaks = 20, 50, 5
+latent_dim = 5
 
-def test_init():
-    n_cells = 20
-    n_genes = 50
-    n_peaks = 5
-    latent_dim = 5
+# Create a random anndata object for RNA.
+rna = ad.AnnData(np.random.rand(n_cells, n_genes))
+rna.var["highly_variable"] = True
 
-    # Create a random anndata object for RNA.
-    rna = ad.AnnData(np.random.rand(n_cells, n_genes))
-    rna.var["highly_variable"] = True
+# Create a random anndata object for ATAC.
+atac = ad.AnnData(np.random.rand(n_cells, n_peaks))
+atac.var["highly_variable"] = True
 
-    # Create a random anndata object for ATAC.
-    atac = ad.AnnData(np.random.rand(n_cells, n_peaks))
-    atac.var["highly_variable"] = True
+# Create a MuData object combining RNA and ATAC.
+mdata = mu.MuData({"rna": rna, "atac": atac})
 
-    # Create a MuData object combining RNA and ATAC.
-    mdata = mu.MuData({"rna": rna, "atac": atac})
+mdata.obs["rna:mod_weight"] = 0.5
+mdata.obs["atac:mod_weight"] = 0.5
+
+
+def test_default_params():
 
     # Initialize the Mowgli model.
-    model = models.MowgliModel(latent_dim=latent_dim)
+    model = models.MowgliModel(
+        latent_dim=latent_dim,
+        cost_path="cost.npy",
+    )
+
+    # Train the model.
+    model.train(mdata)
+
+    # Check the size of the embedding.
+    assert mdata.obsm["W_OT"].shape == (n_cells, latent_dim)
+
+    # Check the size of the dictionaries.
+    assert mdata["rna"].uns["H_OT"].shape == (n_genes, latent_dim)
+    assert mdata["atac"].uns["H_OT"].shape == (n_peaks, latent_dim)
+
+
+def test_custom_params():
+
+    # Initialize the Mowgli model.
+    model = models.MowgliModel(
+        latent_dim=latent_dim,
+        h_regularization={"rna": 0.1, "atac": 0.1},
+        use_mod_weight=True,
+        normalize_rows=True,
+        pca_cost=True,
+        optim_name="adam",
+        cost_path="cost.npy",
+    )
+    model.init_parameters(mdata, force_recompute=True, normalize_rows=True)
 
     # Train the model.
     model.train(mdata)
