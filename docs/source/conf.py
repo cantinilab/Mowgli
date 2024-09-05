46e9a169128aa22e31b655f6e86ea356ae29431c
Geert-Jan Huizing <30904288+gjhuizing@users.noreply.github.com>
2024-09-05 11:52:51 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,8 @@ html_theme = "furo"
 html_title = "Mowgli"
 # html_short_title = "Mowgli"
 
+nbsphinx_allow_errors = True
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
